635a687f94f6f00e46482474504a1e75daef8eb9
sayman <sayman@nixpend.com>
2024-06-08 18:32:51 +0300

    fix bug

--- src/app/logupdate.service.ts
+++ src/app/logupdate.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core'
 import {SwUpdate} from '@angular/service-worker'
+@Injectable({providedIn: 'root'})
+
 export class LogUpdateService {
   constructor(private updates: SwUpdate) {
     updates.versionUpdates.subscribe((evt) => {
